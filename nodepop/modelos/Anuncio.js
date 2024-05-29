0fc544db8715cdf1b5ce4497d6b597735a941b83
Pablo Ruiz <pablo.ruiz.polo@gmail.com>
2024-05-29 14:34:14 +0200

    Arreglada la vista de las etiquetas en la pagina principal, y su correccion

--- nodepop/modelos/Anuncio.js
+++ nodepop/modelos/Anuncio.js
@@ -1,30 +1,28 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // Create de Anuncios schema
 const anuncioSchema = mongoose.Schema({
-    nombre: { type: String, required: true },
-    venta: { type: Boolean, required: true, index: true }, 
-    precio: { type: Number, index: true }, 
-    foto: String, 
-    tags: {
-        type: [String],
-        enum: ['work', 'lifestyle', 'motor', 'mobile']
-    }
+  nombre: { type: String, required: true },
+  venta: { type: Boolean, required: true, index: true },
+  precio: { type: Number, index: true },
+  foto: String,
+  tags: {
+    type: [String],
+    enum: ["work", "lifestyle", "motor", "mobile"],
+  },
 });
 
 //Set the method list
-anuncioSchema.statics.listar = function(filtro, skip, limit, fields) {
-    const query = Anuncio.find(filtro);
-    query.skip(skip);
-    query.limit(limit);
-    query.select(fields);
-    return query.exec()
-}
-
+anuncioSchema.statics.listar = function (filtro, skip, limit, fields) {
+  const query = Anuncio.find(filtro);
+  query.skip(skip);
+  query.limit(limit);
+  query.select(fields);
+  return query.exec();
+};
 
 // Create the anuncios model
-const Anuncio = mongoose.model('Anuncio', anuncioSchema);
-
+const Anuncio = mongoose.model("Anuncio", anuncioSchema);
 
 //Export the model
 module.exports = Anuncio;
