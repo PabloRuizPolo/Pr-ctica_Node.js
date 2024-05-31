1457b9afe30fd505e3a30030631f570e36e867cd
Pablo Ruiz <pablo.ruiz.polo@gmail.com>
2024-05-31 12:20:12 +0200

    Ya puedo mandar img en mi creacion de anuncios y se me crean en la carpeta productImage

---
+++ nodepop/lib/publicUploadConfigure.js
@@ -0,0 +1,16 @@
+const multer = require("multer");
+const path = require("node:path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const rute = path.join(__dirname, "..", "public", "productImage");
+    cb(null, rute);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalName);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
--- nodepop/routes/api/anuncios.js
+++ nodepop/routes/api/anuncios.js
@@ -1,90 +1,87 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Anuncio = require('../../modelos/Anuncio');
+const Anuncio = require("../../modelos/Anuncio");
+const upload = require("../../lib/publicUploadConfigure");
 
 /* GET /api/anuncios
 return anuncios list */
-router.get('/', async function(req, res, next) {
-    try {
-        //filters
-        //http://127.0.0.1:3000/api/anuncios?tags=lifestyle&nombre=bicicleta&venta=true
-        const filterByName = req.query.nombre;
-        const filterByTag = req.query.tags;
-        const filterByVenta = req.query.venta;
+router.get("/", async function (req, res, next) {
+  try {
+    //filters
+    //http://127.0.0.1:3000/api/anuncios?tags=lifestyle&nombre=bicicleta&venta=true
+    const filterByName = req.query.nombre;
+    const filterByTag = req.query.tags;
+    const filterByVenta = req.query.venta;
 
-        //page
-        //http://127.0.0.1:3000/api/anuncios?skip=2&limit=5
-        const skip = req.query.skip;
-        const limit = req.query.limit;
+    //page
+    //http://127.0.0.1:3000/api/anuncios?skip=2&limit=5
+    const skip = req.query.skip;
+    const limit = req.query.limit;
 
-        //field list
-        //http://127.0.0.1:3000/api/anuncios?fields=nombre&fields=tags
-        const fields = req.query.fields
-    
-        const filter = {}
+    //field list
+    //http://127.0.0.1:3000/api/anuncios?fields=nombre&fields=tags
+    const fields = req.query.fields;
 
-        // ?nombre=nombre
-        if (filterByName) {
-            filter.nombre = { $regex: `^${filterByName}`, $options: 'i' };
-        }
-        // ?tags=nombre
-        if (filterByTag) {
-            filter.tags = { $all: filterByTag };
-        }
-        // ?venta=true or false
-        if (filterByVenta === 'true') {
-            filter.venta = true
-        } else if (filterByVenta === 'false') {
-            filter.venta = false
-        }
-        
+    const filter = {};
 
-        const anuncios = await Anuncio.listar(filter, skip, limit, fields);
-        res.json({ results: anuncios});
-
-    } catch (error) {
-        next(error)
+    // ?nombre=nombre
+    if (filterByName) {
+      filter.nombre = { $regex: `^${filterByName}`, $options: "i" };
     }
+    // ?tags=nombre
+    if (filterByTag) {
+      filter.tags = { $all: filterByTag };
+    }
+    // ?venta=true or false
+    if (filterByVenta === "true") {
+      filter.venta = true;
+    } else if (filterByVenta === "false") {
+      filter.venta = false;
+    }
+
+    const anuncios = await Anuncio.listar(filter, skip, limit, fields);
+    res.json({ results: anuncios });
+  } catch (error) {
+    next(error);
+  }
 });
 
 //POST /api/anuncios (body)
 // Create a new anuncio
-router.post('/', async(req, res, next) => {
-    //paso los datos que recibo a variables
-    try {
-        const anuncioInfo = req.body;
-        // creamos una instancia  de agente en memoria
-        const anuncio = new Anuncio(anuncioInfo);
+router.post("/", upload.single("foto"), async (req, res, next) => {
+  //paso los datos que recibo a variables
+  try {
+    const anuncioInfo = req.body;
+    // creamos una instancia  de agente en memoria
+    const anuncio = new Anuncio(anuncioInfo);
 
-        // Lo persistimos (guardamos) en la BD
-        const anuncioSave = await anuncio.save();
+    // Lo persistimos (guardamos) en la BD
+    const anuncioSave = await anuncio.save();
 
-        res.json({result: anuncioSave});
-
-    } catch (error) {
-        next(error)
-    }
+    res.json({ result: anuncioSave });
+  } catch (error) {
+    next(error);
+  }
 });
 
 //GET /api/anuncios/(vender or comprar)
 //show adds to buy with 'comprar' and to sell whit 'vender'
-router.get('/:accion', async (req, res, next) => {
-    try {
-        const accion = req.params.accion
-        let quiero;
-    
-        if (accion === 'vendo') {
-            quiero = true
-        } else if (accion === 'compro') {
-            quiero = false
-        }
-        console.log(quiero)
-        const anuncios = await Anuncio.find({venta: quiero})
-        res.json({results: anuncios})
-    } catch (error) {
-        next(error)
-    }
-})
+router.get("/:accion", async (req, res, next) => {
+  try {
+    const accion = req.params.accion;
+    let quiero;
 
+    if (accion === "vendo") {
+      quiero = true;
+    } else if (accion === "compro") {
+      quiero = false;
+    }
+    console.log(quiero);
+    const anuncios = await Anuncio.find({ venta: quiero });
+    res.json({ results: anuncios });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
